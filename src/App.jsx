7c34a0c674370c5d39781764ee32219ebeb5b5fe
Brendan Lawless <brendanlawless@hotmail.com>
2023-05-27 20:25:54 +0100

    Form state

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,70 @@
 import { useState } from 'react'
-import list from './assets/images/icon-list.svg'
-import success from './assets/images/icon-success.svg'
-import desktop from './assets/images/illustration-sign-up-desktop.svg'
-import mobile from './assets/images/illustration-sign-up-mobile.svg'
+import { useFormik } from 'formik';
+
+const validate = values => {
+  const errors = {};
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+  return errors;
+}
 
 function App() {
   const [isCompleted, setIsCompleted] = useState(false)
+  const [userEmail, setUserEmail] = useState(null)
 
-  const listItems = ['Product discovery and building what matters', 'Measureing to ensure updates are a success', 'and much more!']
+  const formik = useFormik({
+    initialValues: {
+      email: ''
+    },
+    validate,
+    onSubmit: values => {
+      setUserEmail(values.email)
+      setIsCompleted(!isCompleted)
+    },
+  });
+
 
   return (
     <main>
-
-
-
-      {listItems.map(item => {
-        return <li>{item}</li>
-      })}
+      {!isCompleted ?
+        <section>
+          <h1>Stay updated!</h1>
+          <h2>join 60,000+ product managers receiving monthly updates</h2>
+          <ul>
+            <li>Product discovery and building what matters</li>
+            <li>Measureing to ensure updates are a success</li>
+            <li>and much more!</li>
+          </ul>
+          < form onSubmit={formik.handleSubmit}>
+            <label htmlFor="email">Email Address</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+            />
+            {formik.touched.email && formik.errors.email ? (
+              <span>{formik.errors.email}</span>
+            ) : null}
+            <button type="submit">Subscribe to monthly newsletter</button>
+          </form>
+        </section>
+        : <section>
+          <h1>Thank for subscribing!</h1>
+          <p>A confirmation email has been sent to {userEmail}. Please open it and click the button inside to confirm your subscription.</p>
+          <button onClick={() => setIsCompleted(!isCompleted)}>Dimiss message</button>
+        </section>
+      }
     </main>
   )
+
 }
+
+
 
 export default App
